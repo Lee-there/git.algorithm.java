ea741ae8b825afd55cbeb7399cbc42facfe9c359
Lee-there <skyldy8293@naver.com>
2025-12-22 03:07:59 +0000

    update

--- test.java
+++ test.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 public class test{
     public static void main(String[] args){
 
-        Scanner in = new Scanner();
-        System.out.print("HI\n");
+        try (Scanner in = new Scanner(System.in)) {
+    int x = in.nextInt();
+    System.out.println(x);
+}
     }
 }
