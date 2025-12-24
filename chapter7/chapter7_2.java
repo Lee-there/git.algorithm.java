1e21f5f72e7979c32f3762e116f86c60f2e0904f
Lee-there <skyldy8293@naver.com>
2025-12-24 14:25:10 +0000

    update

--- chapter7/chapter7_2.java
+++ chapter7/chapter7_2.java
@@ -1,1 +1,39 @@
 package chapter7;
+import java.util.*;
+
+public class chapter7_2{
+    // 버블정렬 : 복습 필요
+    public void swap(int[] arr, int i, int j){
+        int tmp = arr[i]; 
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public int[] solution(int a , int[] arr){
+        for(int i=0; i<a-1; i++){
+            for(int j=0; j<a-1-i; j++){
+                if(arr[j] > arr[j+1]){
+                    swap(arr,j,j+1);
+                }
+            }
+        }
+          return arr;
+    }
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        chapter7_2 T = new chapter7_2();
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int[] arr = new int[a];
+        for(int i=0; i<a; i++){
+            arr[i] = in.nextInt();
+        }
+        for(int x : T.solution(a,arr)){
+            System.out.print(x + " ");
+        }
+        in.close();
+    }
+
+
+
+}
--- chapter7/chapter7_3.java
+++ chapter7/chapter7_3.java
@@ -1,1 +1,34 @@
 package chapter7;
+import java.util.*;
+
+
+public class chapter7_3{
+    // 삽입정렬 : 복습필요
+    public int[] solution(int a, int[] arr){
+        for(int i=1; i<a;i++){
+            int tmp = arr[i], j;
+            for( j= i-1; j>=0; j--){
+                if(arr[j] > tmp) arr[j+1] = arr[j];
+                else break;
+            }
+            arr[j+1] = tmp;
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        chapter7_3 T = new chapter7_3();
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int[] arr = new int[a];
+        for(int i=0; i<a; i++){
+            arr[i] = in.nextInt();
+        }
+        for(int x : T.solution(a,arr)){
+            System.out.print(x + " ");
+        }
+    }
+
+
+    
+}
