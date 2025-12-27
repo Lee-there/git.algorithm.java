df52fb0cb3cb335b0f2013a63b4b44732bce914b
Lee-there <skyldy8293@naver.com>
2025-12-27 14:32:19 +0000

    update

--- chapter8/chapter8_6.java
+++ chapter8/chapter8_6.java
@@ -1,5 +1,35 @@
 package chapter8;
+import java.util.*;
 
 public class chapter8_6 {
-    
+    /* DFS를 이용해 1부터 n까지를 원소로 갖는 집합의 모든 부분집합을 출력함 */
+    static int n;
+    static int[] ch;
+    public void DFS(int L){
+        if(L == n+1){ // 한 부분집합이 완성됐을 때 그 부분집합을 출력하기 위한 코드
+            String tmp = "";
+            for(int i=1; i<=n; i++){
+                if(ch[i] == 1) tmp += (i+" ");
+            }
+            if(tmp.length()>0) System.out.println(tmp);
+        }
+        else{ // 스택 쌓이는 과정을 천천히 확인하면서 부분집합을 어떻게 구하는지 생각해보자
+            ch[L] = 1;
+            DFS(L+1);
+            ch[L] = 0;
+            DFS(L+1);
+        }
+    }
+
+
+    @SuppressWarnings("ConvertToTryWithResources")
+   public static void main(String[] args) {
+     chapter8_6 T = new chapter8_6();
+    Scanner in = new Scanner(System.in);
+     n = in.nextInt();
+    ch = new int[n+1];
+    T.DFS(1);
+    in.close();
+   }
+
 }
--- chapter8/chapter8_7.java
+++ chapter8/chapter8_7.java
@@ -1,5 +1,50 @@
 package chapter8;
 
+import java.util.*;
+
+  class Node{
+        int data;
+        Node rt, lt;
+        public Node(int val){
+            data = val;
+            lt = rt = null;
+        }
+    }
+
 public class chapter8_7 {
-    
+    /* 레벨탐색 ( 맨 위에서 밑으로 N번 내려가야 존재하는 요소들을 다 정리하는 탐색)
+        이 자료구조를 통해 최단거리 구할 수 있음 (레벨이 제일 낮은 곳에 있는 놈이 아마 최단거리가 되지 않을까 예상)
+     */
+   Node root;
+   public void BFS(Node root){
+    Queue<Node> Q = new LinkedList<>();
+    Q.offer(root);
+    int L = 0;
+    while(!Q.isEmpty()){
+        int len = Q.size();
+        System.out.print(L+" : ");
+        for(int i=0; i<len; i++){
+            Node cur = Q.poll();
+            System.out.print(cur.data+ " ");
+            if(cur.lt != null) Q.offer(cur.lt);
+            if(cur.rt != null) Q.offer(cur.rt);
+        }
+        L++;
+        System.out.println();
+    }
+
+   }
+
+
+    public static void main(String[] args) {
+        chapter8_7 tree = new chapter8_7();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        tree.root.rt.lt = new Node(6);
+        tree.root.rt.rt = new Node(7);
+        tree.BFS(tree.root);
+    }
 }
