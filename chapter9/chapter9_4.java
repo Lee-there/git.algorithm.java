3100f434c8cf744cbf9aef3802da53c3a6573c2c
Lee-there <skyldy8293@naver.com>
2026-01-06 13:21:07 +0000

    update

--- chapter9/chapter9_4.java
+++ chapter9/chapter9_4.java
@@ -1,6 +1,34 @@
 package chapter9;
+import java.util.*;
 
 public class chapter9_4 {
+    // 중복순열 문제
+    static int[] pm;
+    static int n,m;
+    public void DFS(int L){
+        if(L == m){
+            for(int x : pm){
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }else{
+            for(int i=1; i<=n; i++){
+                pm[L] = i;
+                DFS(L+1);
+            }
+            
+        }
 
+    }
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        chapter9_4 T = new chapter9_4();
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        m = in.nextInt();
+        pm = new int[m];
+        T.DFS(0);
+        in.close();
+    }
 }
 
--- chapter9/chapter9_5.java
+++ chapter9/chapter9_5.java
@@ -1,31 +1,13 @@
 package chapter9;
 
 
-// 중복 순열 공부 후 다시 도전해보기
-public class chapter9_5 {
 
+public class chapter9_5 {
+ public static void main(String[] args) {
+    chapter9_5 T = new chapter9_5();
+    
+ }
 }
 
-/*  내가 만들었던 코드
-1. 
-  static int[] arr;
-    static int cost;
-    public int BFS(int n){
-        Queue<Integer> Q = new LinkedList<>();
-        Q.offer(0);
-        int L = 0;
-        while(!Q.isEmpty()){
-            int len = Q.size();
-            for(int i=0; i<len; i++){
-                int v = Q.poll();
-                for(int j=0; j<n; j++){
-                    if(v+arr[j] == cost) return L+1;
-                    if(v+arr[j]<cost){
-                    Q.offer(v+arr[j]);
-                    }
-                }
-            }
-            L++;
-        }
-        return L;
-    }  */
+
+ 
