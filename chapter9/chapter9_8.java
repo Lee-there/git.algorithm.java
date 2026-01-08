c891cbf974b9188effe7a0e61d1d554e8c29c413
Lee-there <skyldy8293@naver.com>
2026-01-08 05:13:39 +0000

    update

--- chapter9/chapter9_8.java
+++ chapter9/chapter9_8.java
@@ -1,5 +1,54 @@
 package chapter9;
+import java.util.*;
+
 
 public class chapter9_8 {
-    
-}
+    static int[] pm, qm, ch;
+    boolean flag = false;
+    int[][] dy = new int[35][35];
+    static int p,q;
+    public int combi(int n , int r){
+        if(dy[n][r] > 0 ) return dy[n][r];
+        if(n==r || r==0) return 1;
+        else return dy[n][r] = combi(n-1,r-1) + combi(n-1,r);
+    }
+    public void DFS(int L , int sum){
+        if(flag) return;   // 시간 복잡도 줄이기 위해 필요
+       if(L == p){
+        if(sum == q){
+        for(int x : qm) System.out.print(x + " ");
+        System.out.println();
+        flag = true;
+        
+        }
+       }else{ //for문으로 수열 만드는 알고리즘임(자주 쓰일듯 함)
+        for(int i=1; i<=p; i++){ //for문 이렇게 사용하면 알아서 사전식이 됨
+            if(ch[i] == 0){
+                ch[i] = 1;
+                qm[L] = i;
+                DFS(L+1, sum + (pm[L]*qm[L]));
+                ch[i] = 0;
+            }
+        }
+
+       }
+        
+    }
+
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        chapter9_8 T = new chapter9_8();
+        Scanner in = new Scanner(System.in);
+        p = in.nextInt();
+        pm = new int[p];
+         for(int i=0; i<p; i++){
+            pm[i] = T.combi(p-1,i);
+        }
+        qm = new int[p];
+        q = in.nextInt();
+        ch = new int[p+1];
+        T.DFS(0,0);
+        in.close();
+        }
+    }
+
--- chapter9/chapter9_9.java
+++ chapter9/chapter9_9.java
@@ -1,5 +1,32 @@
 package chapter9;
+import java.util.*;
 
 public class chapter9_9 {
-    
+    /* 조합 구하기 문제
+       내가 짠 코드에서는 비슷했지만 ch배열을 만들어 비효율적인 코드가 됨 (이미 int S라는 변수를 통해 
+       DFS가 넘어갈 때 마다 방금 추가한 i보다 큰 수만 넣을 수 있게 코드를 짬) -> 중복이 생길 수 없음
+     */
+    static int n, m;
+    static int[]  arr;
+    public void DFS(int L, int S){ // S 는 조합이 추가할 수 있는 숫자 중 최소인 숫자
+        if(L == m){
+            for(int x : arr) System.out.print(x+ " ");
+            System.out.println();
+        }else{
+            for(int i=S; i<=n; i++){ // 다음 DFS에서 i+1부터 탐색하기 때문에 체크 배열을 사용할 필요가 없게 됨
+                    arr[L] = i;
+                    DFS(L+1, i+1); // i가 추가된 순간 그 이후의 숫자만 사용       
+            }
+        }
+    }
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        chapter9_9 T = new chapter9_9();
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        m = in.nextInt();
+        arr = new int[m];
+        T.DFS(0, 1);
+        in.close();
+    }
 }
