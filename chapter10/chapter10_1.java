1f3806e03740f3869e1e612e6b4d0574fa097af6
Lee-there <skyldy8293@naver.com>
2026-01-11 09:57:27 +0000

    update

---
+++ chapter10/chapter10_1.java
@@ -0,0 +1,10 @@
+package chapter10;
+import java.util.*;
+
+public class chapter10_1 {
+    public static void main(String[] args) {
+        chapter10_1 T = new chapter10_1();
+        Scanner in = new Scanner(System.in);
+        
+    }
+}
