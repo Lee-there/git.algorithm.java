55455f40ade4b391bc4668efed125c15fe6e8a12
Lee-there <skyldy8293@naver.com>
2026-01-13 14:31:55 +0000

    update

--- chapter10/chapter10_2.java
+++ chapter10/chapter10_2.java
@@ -1,5 +1,51 @@
 package chapter10;
+import java.util.*;
 
-public class chapter10_2 {
-    
+class Time implements Comparable<Time>{
+    public int s, e;
+    Time(int x, int y){
+        this.s = x;
+        this.e = y;
+    }
+    @Override
+    public int compareTo(Time o){
+        if(this.e==o.e) return this.s - o.s;
+        else return this.e-o.e;
+    }
 }
+public class chapter10_2 {
+    public int solution(ArrayList<Time> arr , int n){
+        int cnt=0;
+        Collections.sort(arr);
+        int et = 0;
+        for(Time x : arr){
+           if(x.s>=et){
+            cnt++;
+            et = x.e;
+           } 
+        }
+        return cnt;
+    }
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        chapter10_2 T = new chapter10_2();
+        Scanner in = new Scanner(System.in);
+        ArrayList<Time> arr = new ArrayList<>();
+        int n = in.nextInt();
+        for(int i=0; i<n; i++){
+            int s = in.nextInt();
+            int e = in.nextInt();
+            arr.add(new Time(s,e));
+        }
+        System.out.println(T.solution(arr,n));
+        in.close();
+    }
+}
+/* 회의실 배정 문제
+    솔직히 그냥 어떻게 풀어야될 지 몰라서 풀이 듣고도 강의 보면서 코드 작성함
+    느낀점 :사실상 제공받은 자료들을 어떻게 잘 정렬해서 순서대로 보느냐가 정말 중요한 듯 
+            솔직히 지금 이 정렬 방식이 왜 예외 없이 최선의 방법인지 감이 안잡힘(반례 있을거 같아서 찝찝함)
+    이번 문제는 회의 끝나는 시간 기준으로 오름차순 후 끝나는 시간 동일 시 시작 시간 오름차순 순으로 정렬
+    이렇게 정렬만 잘 해놓으면 그 다음부터 코드는 쉬운 
+    (자료를 어떻게 정렬해 놓을 것이며 그 정렬을 통해 어떤 방법으로 문제 해결해 나갈지가 중요)
+     -> 솔직히 그냥 이건 많이 풀어보면서 유형 익혀야될듯*/
--- chapter10/chapter10_3.java
+++ chapter10/chapter10_3.java
@@ -1,5 +1,48 @@
 package chapter10;
-
-public class chapter10_3 {
-    
+import java.util.*;
+class Time implements Comparable<Time>{
+    public int time; 
+    public char state;
+    Time(int time , char state){
+        this.time = time;
+        this.state = state;
+    }
+    @Override
+    public int compareTo(Time o){
+        if(this.time == o.time) return this.state - o.state;
+        else return this.time - o.time;
+    }
 }
+public class chapter10_3 {
+    public int solution(ArrayList<Time> arr){
+        int answer = Integer.MIN_VALUE;
+        Collections.sort(arr);
+        int cnt = 0;
+        for(Time o : arr){
+            if(o.state == 's') cnt++;
+            else cnt--;
+            answer = Math.max(answer, cnt);
+        }
+        return answer;
+    }
+    @SuppressWarnings("ConvertToTryWithResources")
+ public static void main(String[] args) {
+    chapter10_3 T = new chapter10_3();
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    ArrayList<Time> arr = new ArrayList<>();
+    for(int i=0; i<n; i++){
+        int s = in.nextInt();
+        int e = in.nextInt();
+        arr.add(new Time(s, 's'));
+        arr.add(new Time(e, 'e'));
+    }
+    System.out.println(T.solution(arr));
+    in.close();
+ }   
+}
+/* 겹치는 최대 인원 수 찾는 문제
+굳이 들어온 시간과 나가는 시간을 짝으로 받는다고 자료를 짝으로 받을 필요 X (대신 다른 방법으로 분류)
+그냥 이 알고리즘은 제공되는 자료들을 어떻게 잘 분류 정렬하는지가 젤 중요한듯 
+**greedy 알고리즘은 일단 많이 풀어보자**(감 익혀야 될듯)
+ */
