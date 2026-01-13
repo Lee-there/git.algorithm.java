cb1d59c88db4b842bf51ac0cb19a3158e4d3e642
Lee-there <skyldy8293@naver.com>
2026-01-13 05:48:51 +0000

    update

--- chapter7/chapter7_8.java
+++ chapter7/chapter7_8.java
@@ -1,4 +1,33 @@
 package chapter7;
+import java.util.*;
+
 public class chapter7_8{
+    //이중검색
+    public int solution(int n , int m, int[] arr){
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 0,  rt = n-1;
+        while(lt<=rt){
+            int mid = (lt+rt)/2;
+            if(arr[mid] == m){
+                answer = mid + 1; // 이 부분 실수 나올 확률 있음
+                break;
+            }
+            if(arr[mid]>m) rt = mid-1;
+            else lt = mid + 1;
+        }
+        return answer;
+    }
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        chapter7_8 T = new chapter7_8();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) arr[i] = in.nextInt();
+        System.out.println(T.solution(n,m,arr)); 
+        in.close();
+    }
     
 }
--- chapter7/chapter7_9.java
+++ chapter7/chapter7_9.java
@@ -1,4 +1,46 @@
 package chapter7;
+import java.util.*;
+
 public class chapter7_9{
+    /*결정 알고리즘
+        이중 검색을 통해 최선의 답 찾기
+     */
+    public int count(int[] arr, int c){
+        int cnt = 1;
+        int sum = 0;
+        for(int x : arr){
+            if(sum+x > c){
+                cnt++;
+                sum = x;
+            }
+            else sum+= x;
+        }
+        return cnt;
+    }
     
+    public int solution(int n, int m, int[] arr){
+        int answer = 0;
+        int lt = Arrays.stream(arr).max().getAsInt();
+        int rt = Arrays.stream(arr).sum();
+        while(lt<=rt){
+            int mid = (lt+rt)/2;
+            if(count(arr,mid)<=m){
+                answer = mid;
+                rt = mid-1;
+            }else lt = mid+1;
+        }
+
+        return answer;
+    }
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        chapter7_9 T = new chapter7_9();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) arr[i] = in.nextInt();
+        System.out.println(T.solution(n,m,arr));
+        in.close();
+    }
 }
